20930e9d56f7258830f7354bfafec9593b44b367
Shinye Song <shinye.song@croquis.com>
2020-12-18 01:49:29 +0900

    improve configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,10 +66,11 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/favicon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
