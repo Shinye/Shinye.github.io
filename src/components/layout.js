b280237dddfc14dc7b83020071f90b1095d166a8
Shinye Song <shinye.song@croquis.com>
2020-12-18 01:50:43 +0900

    edit layouts

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,33 +1,33 @@
 import React from "react"
 import { Link } from "gatsby"
 
+const logo = require('../../static/images/logo.svg');
+const styles = require('../styles/common/layout.scss');
+
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
 
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
+  const header = (
+    <h1 className="main-heading">
+      <Link to="/">
+        <img src={logo} className={styles.logo}/>
+        <div className={styles.menu}>
+          <Link className={styles.menu_item} to="/">Root</Link>
+          <Link className={styles.menu_item} to="/">Peace</Link> 
+          <Link className={styles.menu_item} to="/">Love</Link> 
+          <Link className={styles.menu_item} to="/about">About</Link> 
+        </div>
       </Link>
-    )
-  }
+    </h1>
+  )
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        © {new Date().getFullYear()}, eternal peace, bunch of love :)
       </footer>
     </div>
   )
